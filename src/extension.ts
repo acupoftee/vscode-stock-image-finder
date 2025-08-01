de08356fad0d8e7258b3f4e3f705ed6925ab089f
acupoftee <teedia31@gmail.com>
2025-08-01 14:01:33 -0400

    webview test

--- src/extension.ts
+++ src/extension.ts
@@ -11,6 +11,16 @@ import * as vscode from "vscode";
 export function activate(context: vscode.ExtensionContext) {
   console.log('"stock-image-fimder" is now active!');
 
+  // Technical Guidelines
+  // All API uses must use the hotlinked image URLs returned by the API under the photo.urls  properties.
+  // This applies to all uses of the image and not just search results.
+
+  // When your application performs something similar to a download (like when a user chooses the image to include in a blog post, set as a header, etc.),
+  // you must send a request to the download endpoint returned under the photo.links.download_location  property.
+
+  // When displaying a photo from Unsplash, your application must attribute Unsplash, the Unsplash photographer, and contain a link back to their Unsplash profile.
+  // All links back to Unsplash should use utm parameters in the ?utm_source=your_app_name&utm_medium=referral.
+
   const imageSearchCommand = vscode.commands.registerCommand(
     "stock-image-finder.findUnsplashImage",
     async () => {
@@ -22,11 +32,94 @@ export function activate(context: vscode.ExtensionContext) {
         vscode.window.showWarningMessage(`Please include valid search query`);
         return;
       }
-      vscode.window.showInformationMessage(`Unsplash query: ${query}`);
+      // Creates a new webview panel beside the active panel
+      const panel = vscode.window.createWebviewPanel(
+        "stock-image-finder",
+        `Unsplash Images for ${query}`,
+        vscode.ViewColumn.Beside,
+        {}
+      );
+      panel.webview.html = getWebviewContent();
     }
   );
 
   context.subscriptions.push(imageSearchCommand);
 }
 
+function getWebviewContent() {
+  return `
+  <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Image Gallery</title>
+    <style>
+        .images {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 12px;
+}
+
+.image {
+    width: auto;
+    height: 250px;
+    background-color: pink;
+}
+
+.pagination {
+    display: flex;
+    width: auto;
+    justify-content: cener;
+}
+    </style>
+</head>
+<body>
+    <div class="image-grid">
+        <div class="images">
+            <div class="image"></div>
+            <div class="image"></div>
+            <div class="image"></div>
+            <div class="image"></div>
+            <div class="image"></div>
+            <div class="image"></div>
+            <div class="image"></div>
+            <div class="image"></div>
+            <div class="image"></div>
+            <div class="image"></div>
+            <div class="image"></div>
+            <div class="image"></div>
+            <div class="image"></div>
+            <div class="image"></div>
+            <div class="image"></div>
+            <div class="image"></div>
+            <div class="image"></div>
+            <div class="image"></div>
+            <div class="image"></div>
+            <div class="image"></div>
+            <div class="image"></div>
+            <div class="image"></div>
+            <div class="image"></div>
+            <div class="image"></div>
+            <div class="image"></div>
+            <div class="image"></div>
+            <div class="image"></div>
+            <div class="image"></div>
+            <div class="image"></div>
+            <div class="image"></div>
+        </div>
+        <div class="pagination-controls">
+            <button disabled>Previous</button>
+            <button>Next</button>
+        </div>
+    </div>
+    <div class="selected-image">
+        <div class="image-container"></div>
+        <div class="image-caption"></div>
+        <div class="image-code-snippet"></div>
+    </div>
+</body>
+</html>
+  `;
+}
 export function deactivate() {}
